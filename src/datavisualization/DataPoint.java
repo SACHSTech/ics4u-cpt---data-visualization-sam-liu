1b1fc3e01ac57026026a5b94011dfd819050edb2
samliu1234 <64169932+samliu1234@users.noreply.github.com>
2021-01-14 14:13:04 -0500

    created DataPoint class

--- src/datavisualization/DataPoint.java
+++ src/datavisualization/DataPoint.java
@@ -6,4 +6,14 @@ class DataPoint {
     int year;
     double crimeIndex;
 
+    DataPoint(String province, int year, double crimeIndex) {
+        this.province = province;
+        this.year = year;
+        this.crimeIndex  = crimeIndex;
+    }
+
+    public String toString() {
+        return "Province: " + province + ", Year: " + year + ", Crime index: " + crimeIndex;
+    }
+
 }
--- src/datavisualization/DataVisualization.java
+++ src/datavisualization/DataVisualization.java
@@ -1,6 +1,8 @@
 package datavisualization;
 
 import java.io.*;
+import java.util.*;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,14 +16,30 @@ public class DataVisualization extends Application {
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Crime index over time");
 
+        ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
+
         BufferedReader file = new BufferedReader(new FileReader("data.csv"));
         String strLine;
-        strLine = "";
+        // Reads first line of csv file (e.g. headings)
+        strLine = file.readLine();
         while (strLine != null) {
             strLine = file.readLine();
-            System.out.println(strLine);
+            if (strLine == null || strLine.equals("")) {
+                break;
+            }
+            String[] split = strLine.split(",");
+            for (int i = 0; i < split.length; ++i) {
+                split[i] = split[i].replace("\"", "");
+                System.out.println(split[i]);
+            }
+            System.out.println(split.length);
+            dataPoints.add(new DataPoint(split[1], Integer.parseInt(split[0]), Double.parseDouble(split[2])));
         }
         file.close();
+        
+        for (DataPoint d: dataPoints) {
+            System.out.println(d);
+        }
 
         primaryStage.show();
     }
