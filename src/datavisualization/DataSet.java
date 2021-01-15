2a8d8846df87f342cc73482ab52c63064a2a2be3
samliu1234 <64169932+samliu1234@users.noreply.github.com>
2021-01-14 20:00:16 -0500

    finished sort implementation

---
+++ src/datavisualization/DataSet.java
@@ -0,0 +1,146 @@
+package datavisualization;
+
+import java.util.*;
+
+public class DataSet {
+    
+    private ArrayList<DataPoint> dataPoints;
+    private int size;
+
+    public DataSet(ArrayList<DataPoint> dataPoints) {
+        this.dataPoints = dataPoints;
+        this.size = dataPoints.size();
+    }
+
+    public ArrayList<DataPoint> getDataPoints() {
+        return dataPoints;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void addData(DataPoint data) {
+        dataPoints.add(data);
+    }
+
+    /*
+    private void print() {
+        for (DataPoint data: dataPoints) {
+            System.out.println(data);
+        }
+    }
+    */
+
+    public void sort(String sortBy, boolean reverse) {
+        mergeSort(new ArrayList<DataPoint>(dataPoints), 0, size - 1, sortBy, reverse);
+    }
+
+    public DataSet search(String key) {
+        ArrayList<DataPoint> tempList;
+        DataSet searched;
+
+        tempList = new ArrayList<DataPoint>();
+
+        for (DataPoint data: dataPoints) {
+            if (!isNumeric(key) && data.getProvince().contains(key)) {
+                tempList.add(data);
+            }
+            if (isNumeric(key) && (data.getYear() == Double.parseDouble(key) || data.getCrimeIndex() == Double.parseDouble(key)))
+                tempList.add(data);
+            }
+        }
+
+        searched = new DataSet
+
+        return searched;
+    }
+
+    private boolean isNumeric(String key) {
+        try {
+            Double.parseDouble(key);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void mergeSort(ArrayList<DataPoint> temporaryList, int from, int to, String sortBy, boolean reverse) {
+        if (to - from >= 1) {
+            int mid = (from + to) / 2;
+            // Recursive call
+            mergeSort(temporaryList, from, mid, sortBy, reverse);
+            mergeSort(temporaryList, mid + 1, to, sortBy, reverse);
+
+            merge(temporaryList, from, mid, to, sortBy, reverse);
+        }
+    }
+
+    private void merge(ArrayList<DataPoint> temporaryList, int from, int mid, int to, String sortBy, boolean reverse) {
+        int p1;
+        int p2;
+        int p3;
+
+        p1 = from;
+        p2 = mid + 1;
+        p3 = from;
+
+        // Note that sorting by province, year, and crime index is the same function besides the if statement in the initial for loop
+        // So, we merely need to create a boolean that varies for each category
+        while (p1 <= mid && p2 <= to) {
+            boolean isTrue;
+            if (sortBy.equals("province")) {
+                if (reverse) { // Sort by province from z -> a
+                    isTrue = dataPoints.get(p1).getProvince().compareTo(dataPoints.get(p2).getProvince()) > 0;
+                }
+                else { // Sort by province from a -> z
+                    isTrue = dataPoints.get(p1).getProvince().compareTo(dataPoints.get(p2).getProvince()) < 0;
+                }
+            }
+            else if (sortBy.equals("year")) {
+                if (reverse) { // Sort by year from greatest to least
+                    isTrue = dataPoints.get(p1).getYear() > dataPoints.get(p2).getYear();
+                }
+                else { // Sort by year from least to greatest
+                    isTrue = dataPoints.get(p1).getYear() < dataPoints.get(p2).getYear();
+                }
+            }
+            else {
+                if (reverse) { // Sort by crime index from greatest to least
+                    isTrue = dataPoints.get(p1).getCrimeIndex() > dataPoints.get(p2).getCrimeIndex();
+                }
+                else { // Sort by crime index from least to greatest
+                    isTrue = dataPoints.get(p1).getCrimeIndex() < dataPoints.get(p2).getCrimeIndex();
+                }
+            }
+
+            if (isTrue) {
+                temporaryList.set(p3, dataPoints.get(p1));
+                ++p1;
+            }
+            else {
+                temporaryList.set(p3, dataPoints.get(p2));
+                ++p2;
+            }
+            ++p3;
+        }
+
+        while (p1 <= mid) {
+            temporaryList.set(p3, dataPoints.get(p1));
+            ++p1;
+            ++p3;
+        }
+
+        while (p2 <= to) {
+            temporaryList.set(p3, dataPoints.get(p2));
+            ++p2;
+            ++p3;
+        }
+
+        for (int i = from; i <= to; ++i) {
+            dataPoints.set(i, temporaryList.get(i));
+        }
+    }
+
+}
--- src/datavisualization/DataVisualization.java
+++ src/datavisualization/DataVisualization.java
@@ -4,10 +4,13 @@ import java.io.*;
 import java.util.*;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
@@ -16,27 +19,25 @@ public class DataVisualization extends Application {
 
     // Instance variables
     private ScrollPane scrollPane;
-    private ArrayList<DataPoint> dataPoints;
     private GridPane grid;
+    private DataSet wholeDataSet;
 
     public static void main(String args[]) {
         launch(args);
     }
 
+    // TECHNICALLY, ALL VARIABLES SHOULD BE DECLARED AT THE TOP OF THE METHOD
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Crime index over time");
 
-        // Initialize scroll pane
+        // Initialize variables
         scrollPane = new ScrollPane();
-
-        // Obtain data from file
-        dataPoints = new ArrayList<DataPoint>();
-        importData(dataPoints);
-
-        // Initialize grid
         grid = new GridPane();
         scrollPane.setContent(grid);
+
+        wholeDataSet = new DataSet(importData());
 
         // Constraints
         grid.setAlignment(Pos.CENTER);
@@ -45,8 +46,8 @@ public class DataVisualization extends Application {
         grid.setPadding(new Insets(25, 25, 25, 25));
         grid.setGridLinesVisible(true);
         
-        for (int i = 0; i < dataPoints.size(); ++i) {
-            DataPoint data = dataPoints.get(i);
+        for (int i = 0; i < wholeDataSet.getSize(); ++i) {
+            DataPoint data = wholeDataSet.getDataPoints().get(i);
 
             // Province label
             Label province = new Label(data.getProvince());
@@ -57,9 +58,20 @@ public class DataVisualization extends Application {
             grid.add(year, 1, i);
 
             // Crime index label
-            Label crimeIndex = new Label(String.valueOf(data.getYear()));
+            Label crimeIndex = new Label(String.valueOf(data.getCrimeIndex()));
             grid.add(crimeIndex, 2, i);
         }
+
+        Button sortCrimeButton = new Button("Sort by crime index");
+        sortCrimeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("test");
+                wholeDataSet.sort("year", false);
+            }
+        });
+
+        grid.add(sortCrimeButton, 4, 0);
         
         // Create and set scene
         Scene scene = new Scene(scrollPane, 600, 600);
@@ -68,11 +80,14 @@ public class DataVisualization extends Application {
         primaryStage.show();
     }
 
-    private static void importData(ArrayList<DataPoint> dataPoints) throws IOException {
+    private static ArrayList<DataPoint> importData() throws IOException {
         
         BufferedReader file = new BufferedReader(new FileReader("data.csv"));
 
+        ArrayList<DataPoint> temporaryList;
         String strLine;
+
+        temporaryList = new ArrayList<DataPoint>();
         // Reads first line of csv file (e.g. headings)
         strLine = file.readLine();
 
@@ -85,10 +100,12 @@ public class DataVisualization extends Application {
             for (int i = 0; i < split.length; ++i) {
                 split[i] = split[i].replace("\"", "");
             }
-            dataPoints.add(new DataPoint(split[1], Integer.parseInt(split[0]), Double.parseDouble(split[2])));
+            temporaryList.add(new DataPoint(split[1], Integer.parseInt(split[0]), Double.parseDouble(split[2])));
         }
 
         file.close();
+
+        return temporaryList;
 
     }
 
